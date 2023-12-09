02d21e8566dde27f0995a6dc99dc8723c40a4021
Chairulyf <chairulyusuf29@gmail.com>
2023-12-09 19:01:53 +0700

    update

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -6,9 +6,7 @@ import {
 } from "../validation/user-validation.js";
 import { validation} from "../validation/validation.js";
 import {prismaClient} from "../application/database.js";
-import bcrypt from "bcrypt"
 import {ResponseError} from "../error/response-error.js";
-import {v4 as uuid} from "uuid";
 
 const register = async (req) => {
     const user =  validation (registerUserValidation, req);
@@ -22,7 +20,6 @@ const register = async (req) => {
     if (countUser === 1) {
         throw new ResponseError(400, "Username already exists");
     }
-    // user.password = await bcrypt.hash(user.password, 10);
 
     return prismaClient.user.create({
         data: user
@@ -46,25 +43,6 @@ const login = async (req, res)=>{
     if (!user || user.password !==loginRequest.password) {
         throw new ResponseError(401, "Username or password wrong");
     }
-
-    //
-    // const isPasswordValid = await bcrypt.compare(loginRequest.password, user.password);
-    // if (!isPasswordValid) {
-    //     throw new ResponseError(401, "Username or password wrong");
-    // }
-
-    // const token = uuid().toString()
-    // return prismaClient.user.update({
-    //     data: {
-    //         token: token
-    //     },
-    //     where: {
-    //         username: user.username
-    //     },
-    //     select: {
-    //         token: true
-    //     }
-    // });
 }
 
 const booking = async (req)=>{
@@ -99,18 +77,6 @@ const booking = async (req)=>{
 
 const get = async (contactId) => {
     contactId = validation(getContactValidation, contactId)
-
-    // const contact = await prismaClient.contact.findFirst({
-    //     where: {
-    //         id: contactId
-    //     },
-    //     select: {
-    //         id: true,
-    //         full_name: true,
-    //         phone: true,
-    //         email: true
-    //     }
-    // });
 
     const contact = await prismaClient.contact.findFirst({
         where: {
