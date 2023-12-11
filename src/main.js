eab138a0904e8be00b2e30d356dd8d52e1df3eeb
tiara <tiaraoryza111@gmail.com>
2023-12-11 12:11:13 +0700

    memperbarui app port pada main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import {web} from "./application/web.js";
 import {logger} from "./application/logging.js";
+import { application } from "express";
 
 web.listen(3000, ()=>{
     logger.info("app start")
+})
+
+application.listen(process.env.PORT||3000, ()=>{
+    console.log('Server is running on 3000!')
 })
