885ad16a0e57e71de6209e2450e3791fc18e1106
tiara <tiaraoryza111@gmail.com>
2023-12-11 12:16:52 +0700

    memperbarui main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ import {web} from "./application/web.js";
 import {logger} from "./application/logging.js";
 import { application } from "express";
 
-web.listen(3000, ()=>{
+web.listen(process.env.PORT||3000, ()=>{
     logger.info("app start")
 })
 
-application.listen(process.env.PORT||3000, ()=>{
-    console.log('Server is running on 3000!')
-})
